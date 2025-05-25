f9ae1c18411a12785b6587482beec8f46a4be6ad
t0twn <t.t.w.n@outlook.com>
2025-05-25 20:11:10 +0800

    MAP seems to be much simpler

--- generate.py
+++ generate.py
@@ -1,68 +1,145 @@
-import sys
-import re
-
-import template
-
-
-section_domain_backend = "B_{domain}"
-
-sections = {
-    "global": {},
-    "defaults": {},
-    "F_tcp_80": {
-        "acl": "acl is_{domain} hdr_dom(host) -i -m beg {domain}",
-        "use_backend": "use_backend B_redirect if is_{domain}"
-    },
-    "F_tcp_443": {
-        "acl": "acl is_{domain} req_ssl_sni -i {domain}",
-        "use_backend": "use_backend B_{domain} if is_{domain}"
-    },
-    "B_redirect": {
-        "redirect": "redirect scheme https if {{ hdr(Host) -i {domain} }}"
-    },
-    "B_default_web": {},
-    section_domain_backend: "server {server_name} {localhost}:{server_port_local}"
-}
+import pathlib
+import argparse
+import subprocess
 
 
-def generate(servers_info):
-    ha_conf_content = ""
-    indent_space = "        "
-    line_terminate = "\n"
+global ARGS
 
-    for section in sections:
-        if section == section_domain_backend:
-            continue
-        ha_conf_content += getattr(template, f"section_{section}")
-        for partition in sections[section]:
-            ha_conf_content += line_terminate
-            for server_info in servers_info:
-                domain = server_info[1]
-                ha_conf_content += indent_space + sections[section][partition].format(domain=domain) + line_terminate
 
-        ha_conf_content += line_terminate + line_terminate
+CFG_DIR = pathlib.Path("/etc/haproxy")
+CFG_FILE = CFG_DIR / "haproxy.cfg"
 
-    for server_info in servers_info:
-        server_name, domain, server_port_local = server_info
-        section_name = "backend " + section_domain_backend.format(domain=domain) + line_terminate
-        section_content = indent_space + sections[section_domain_backend].format(
-            server_name=server_name, localhost=template.localhost, server_port_local=server_port_local) + line_terminate
-        ha_conf_content += section_name + section_content
-        ha_conf_content += line_terminate + line_terminate
+MAP_DIR = CFG_DIR / "map"
+MAP_301_FILE = MAP_DIR / "301"
+MAP_BACKEND_FILE = MAP_DIR / "backend"
 
-    return ha_conf_content
+BACKEND_CONF = "        server {name} {host}:{port}"
+BACKEND_FILE = "/etc/haproxy/x_{sni}.cfg"
+
+
+def prepare():
+    MAP_DIR.mkdir(exist_ok=True)
+    MAP_301_FILE.touch(exist_ok=True)
+    MAP_BACKEND_FILE.touch(exist_ok=True)
+
+
+def parse_args():
+
+    global ARGS
+
+    parser = argparse.ArgumentParser()
+
+    action_group = parser.add_mutually_exclusive_group(required=True)
+    action_group.add_argument(
+        "--add",
+        "-a",
+        action="store_true",
+        help="Add a backend",
+    )
+    action_group.add_argument(
+        "--del",
+        "-d",
+        action="store_true",
+        help="Del a backend",
+    )
+
+    parser.add_argument(
+        "--name",
+        "-n",
+        required=True,
+        help="Backend server name",
+    )
+    parser.add_argument(
+        "--sni",
+        "-s",
+        required=True,
+        help="Server name indicator mapped to its backend",
+    )
+    parser.add_argument(
+        "--host",
+        "-H",
+        required=True,
+        help="Backend server host",
+    )
+    parser.add_argument(
+        "--port",
+        "-p",
+        required=True,
+        help="Backend server port",
+    )
+    parser.add_argument(
+        "--transparent",
+        "-t",
+        default=False,
+        action="store_true",
+        help="Enable transparent bind",
+    )
+    ARGS = parser.parse_args()
+
+
+def map_301():
+    line = f"{ARGS.sni} 301"
+    cmd = f"grep -qxF '{ARGS.sni} 301' {MAP_301_FILE} || echo '{ARGS.sni} 301' >> {MAP_301_FILE}" if ARGS.add else \
+      f"sed -i '/{line}/d' {MAP_301_FILE}"
+    subprocess.getstatusoutput(cmd)
+
+
+def map_backend():
+    line = f"{ARGS.sni} B_{ARGS.sni}"
+    cmd = f"grep -qxF '{line}' {MAP_BACKEND_FILE} || echo '{line}' >> {MAP_BACKEND_FILE}" if ARGS.add else \
+      f"sed -i '/{line}/d' {MAP_BACKEND_FILE}"
+    subprocess.getstatusoutput(cmd)
+
+
+def cfg_backend():
+    backend_file = BACKEND_FILE.format(sni=ARGS.sni)
+    if ARGS.add:
+        backend_head = f"backend B_{ARGS.sni}"
+        backend_conf = BACKEND_CONF.format(name=ARGS.name, host=ARGS.host, port=ARGS.port)
+        lines = [backend_head, backend_conf]
+        with open(backend_file, "w") as f:
+            for line in lines:
+                f.write(line + "\n")
+        return
+    cmd = f"rm -f {backend_file}"
+    subprocess.getstatusoutput(cmd)
+
+
+def cfg_main():
+    transparent_wildcard = "bind 0.0.0.0:443 transparent"
+    transparent_wildcard_v6 = "bind :::443 v6only transparent"
+
+    non_transparent_wildcard = "bind 0.0.0.0:443"
+    non_transparent_wildcard_v6 = "bind :::443 v6only"
+
+    source = "        source 0.0.0.0 usesrc clientip"
+    default_web_ssl = "server default_web_ssl"
+
+    ensure_transparent_wildcard = f"grep -qF '{transparent_wildcard}' {CFG_FILE} || sed -i 's/{non_transparent_wildcard}*/{transparent_wildcard}/' {CFG_FILE}"
+    ensure_transparent_wildcard_v6 = f"grep -qF '{transparent_wildcard_v6}' {CFG_FILE} || sed -i 's/{non_transparent_wildcard_v6}*/{transparent_wildcard_v6}/' {CFG_FILE}"
+
+    ensure_non_transparent_wildcard = f"grep -qF '{transparent_wildcard}' {CFG_FILE} && sed -i 's/{transparent_wildcard}*/{non_transparent_wildcard}/' {CFG_FILE}"
+    ensure_non_transparent_wildcard_v6 = f"grep -qF '{transparent_wildcard_v6}' {CFG_FILE} && sed -i 's/{transparent_wildcard_v6}*/{non_transparent_wildcard_v6}/' {CFG_FILE}"
+
+    ensure_source = f"grep -qF '{source}' {CFG_FILE} || sed -i '/{default_web_ssl}/i \{source}' {CFG_FILE}"
+    ensure_non_source = f"grep -qF '{source}' {CFG_FILE} && sed -i '/{source}/d' {CFG_FILE}"
+
+    cmd_list = [ensure_transparent_wildcard, ensure_transparent_wildcard_v6, ensure_source] if ARGS.transparent else \
+        [ensure_non_transparent_wildcard, ensure_non_transparent_wildcard_v6, ensure_non_source]
+    for cmd in cmd_list:
+        subprocess.getstatusoutput(cmd)
 
 
 def main():
-    script = sys.argv.pop(0)
-    regex = "(?P<server_name>.+),(?P<domain>.+):(?P<server_port_local>.+)"
-    pattern = re.compile(regex)
-    servers_info = [pattern.match(server_info).groups() for server_info in sys.argv if pattern.match(server_info)]
 
-    if not servers_info:
-        print(f"Usage: python {script} server_name,domain:server_port_local")
-    else:
-        print(generate(servers_info))
+    parse_args()
+
+    prepare()
+    cfg_main()
+    cfg_backend()
+    map_backend()
+    map_301()
+
 
 if __name__ == "__main__":
     main()
